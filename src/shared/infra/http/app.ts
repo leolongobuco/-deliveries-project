2c959779ef25dbbcc4b61c553df14039d3b7824e
Leonardo Longobuco <longobucoleonardo@gmail.com>
2022-10-26 11:24:12 -0300

    tratando exceções e erros

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,5 @@
-import express from "express";
-
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import { router } from "./routes";
 
 const app = express();
@@ -8,4 +8,18 @@ app.use(express.json());
 
 app.use(router);
 
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
+);
 export { app };
