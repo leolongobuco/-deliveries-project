8facd8ef3c339d82cfe41e55861cfab4ab6b0fe4
Leonardo Longobuco <longobucoleonardo@gmail.com>
2022-10-29 23:13:50 -0300

    fix: bug de create client e deliveryman

--- src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
@@ -11,6 +11,7 @@ class CreateDeliverymanUseCase {
     const deliverymanExist = await prisma.deliveryman.findFirst({
       where: {
         username: {
+          equals: username,
           mode: "insensitive",
         },
       },
